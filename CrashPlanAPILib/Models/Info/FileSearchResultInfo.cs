6db3cf8c9ae977cb05ddc2faba94b3d8f91679ce
Elio Gonzalez <elio@opiliosoft.com>
2016-07-08 16:02:42 -0400

    forgot a file in the commit... http://www.youbrokethebuild.com

--- CrashPlanAPILib/Models/Info/FileSearchResultInfo.cs
+++ CrashPlanAPILib/Models/Info/FileSearchResultInfo.cs
@@ -1,14 +1,23 @@
-﻿namespace CrashPlanAPILib.Models.Info
+﻿using System;
+
+namespace CrashPlanAPILib.Models.Info
 {
     public class FileSearchResultInfo
     {
         public string Filename { get; set; }
         public string Path { get; set; }
         public string Id { get; set; }
-        public string LastModified { get; set; }
+        public long LastModified { get; set; }
         public bool Deleted { get; set; }
         public bool Hidden { get; set; }
-        public string Date { get; set; }
+        public DateTime? Date { get; set; }
         public string Type { get; set; }
+
+        public DateTime LastModifedAsDateTime()
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            epoch = epoch.AddMilliseconds(LastModified);
+            return epoch;
+        }
     }
 }
